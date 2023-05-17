c23ba21d3712073f2e3738f17ed33cb32b455967
Vishal <vishal.rathore@multitech.com>
2023-05-17 15:16:52 -0500

    added tailwindcss and basic button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import { ReactElement, useState } from 'react';
-import reactLogo from './assets/react.svg';
+import Button from './components/base/Button';
+import { ReactElement } from 'react';
 import './App.css';
 
 function App(): ReactElement {
-  const [count, setCount] = useState(0);
-
-  return <div>Welcome</div>;
+  return (
+    <div>
+      <Button primary>Button</Button>
+      <Button secondary>Buy now</Button>
+      <Button success>See Deal</Button>
+      <Button warning>Hide Ads</Button>
+      <Button danger>Something</Button>
+      <Button rounded>Rounded</Button>
+      <Button outline>Outline</Button>
+      <Button outline primary>
+        outline primary
+      </Button>
+      <Button outline secondary>
+        outline secondary
+      </Button>
+      <Button outline success>
+        outline success
+      </Button>
+      <Button outline warning>
+        outline warning
+      </Button>
+      <Button outline danger>
+        outline danger
+      </Button>
+    </div>
+  );
 }
 
 export default App;
