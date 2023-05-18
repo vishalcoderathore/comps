d6ca8b36b4ceb4269dd83926d71a00b60c83ce86
Vishal Rathore <vishal.rathore@multitech.com>
2023-05-18 17:22:24 -0500

    finished accordion component

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,8 @@
-import Accordion from './components/base/Accordion';
 import { ReactElement } from 'react';
 import './App.css';
 
 function App(): ReactElement {
-  const items = [
-    {
-      id: Math.random(),
-      label:
-        '[LABEL 1] : Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem illum, mollitia numquam debitis iusto repellat ea quae dicta. Natus labore aut mollitia at tempore veritatis blanditiis, beatae exercitationem similique rem?',
-      content:
-        '[CONTENT 1] : Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa quisquam dolore, delectus quo sed rerum soluta quaerat cupiditate, qui tempora sequi quia corporis ratione porro adipisci, natus vel vero!',
-    },
-    {
-      id: Math.random(),
-      label:
-        '[LABEL 2] : Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem illum, mollitia numquam debitis iusto repellat ea quae dicta. Natus labore aut mollitia at tempore veritatis blanditiis, beatae exercitationem similique rem?',
-      content:
-        '[CONTENT 2] : Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa quisquam dolore, delectus quo sed rerum soluta quaerat cupiditate, qui tempora sequi quia corporis ratione porro adipisci, natus vel vero!',
-    },
-    {
-      id: Math.random(),
-      label:
-        '[LABEL 3] : Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem illum, mollitia numquam debitis iusto repellat ea quae dicta. Natus labore aut mollitia at tempore veritatis blanditiis, beatae exercitationem similique rem?',
-      content:
-        '[CONTENT 3] : Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa quisquam dolore, delectus quo sed rerum soluta quaerat cupiditate, qui tempora sequi quia corporis ratione porro adipisci, natus vel vero!',
-    },
-    {
-      id: Math.random(),
-      label:
-        '[LABEL 4] : Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem illum, mollitia numquam debitis iusto repellat ea quae dicta. Natus labore aut mollitia at tempore veritatis blanditiis, beatae exercitationem similique rem?',
-      content:
-        '[CONTENT 4] : Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa quisquam dolore, delectus quo sed rerum soluta quaerat cupiditate, qui tempora sequi quia corporis ratione porro adipisci, natus vel vero!',
-    },
-  ];
-
-  return (
-    <div>
-      <Accordion items={items} />
-    </div>
-  );
+  return <div>Components</div>;
 }
 
 export default App;
--- src/components/base/Accordion.tsx
+++ src/components/base/Accordion.tsx
@@ -12,30 +12,34 @@ interface AccordionProps {
 }
 
 const Accordion: React.FC<AccordionProps> = ({ items }) => {
-  const [expandedIndex, setExpandedIndex] = useState(0);
+  const [expandedIndex, setExpandedIndex] = useState(-1);
 
   const handleClick = (nextIndex: number): void => {
-    setExpandedIndex(nextIndex);
+    setExpandedIndex(prevIndex => (nextIndex === prevIndex ? -1 : nextIndex));
   };
 
   const displayData = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
     const icon = (
-      <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+      <span className='text-2xl'>
+        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
     );
     return (
       <div key={item.id}>
         {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
-        <div onClick={(): void => handleClick(index)}>
-          {icon}
+        <div
+          className='flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer'
+          onClick={(): void => handleClick(index)}>
           {item.label}
+          {icon}
         </div>
-        {isExpanded && <div>{item.content}</div>}
+        {isExpanded && <div className='border-b p-5'>{item.content}</div>}
       </div>
     );
   });
 
-  return <div>{displayData}</div>;
+  return <div className='border-x border-t rounded'>{displayData}</div>;
 };
 
 export default Accordion;
