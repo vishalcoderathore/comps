33d3d2a827a09a219bf13e4f69445ad8f31bd7e3
Vishal Rathore <vishal.rathore@multitech.com>
2023-05-18 20:52:41 -0500

    dropdown now uses panel component as a wrapper, styling added

--- src/components/base/Dropdown.tsx
+++ src/components/base/Dropdown.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { GoChevronDown } from 'react-icons/go';
+import Panel from './Pannel';
 
 export interface DropdownOption {
   label: string;
@@ -9,11 +11,17 @@ interface DropdownProps {
   options: DropdownOption[];
   value: DropdownOption | null;
   onChange: (option: DropdownOption) => void;
+  selectLabel?: string;
 }
 
-const Dropdown: React.FC<DropdownProps> = ({ options, value, onChange }) => {
+const Dropdown: React.FC<DropdownProps> = ({
+  options,
+  value,
+  onChange,
+  selectLabel = 'Select...',
+}) => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const divEl = useRef<HTMLDivElement | null>(null);
   const handleClick = (): void => {
     setIsOpen(!isOpen);
   };
@@ -23,18 +31,39 @@ const Dropdown: React.FC<DropdownProps> = ({ options, value, onChange }) => {
     onChange(option);
   };
 
+  useEffect(() => {
+    const handler = (event: MouseEvent): void => {
+      if (divEl.current && !divEl.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener('click', handler, true);
+    // Cleanup function to remove event listener when component unmounts
+    return (): void => {
+      document.removeEventListener('click', handler);
+    };
+  }, []);
+
   const renderedOptions = options.map(option => (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-    <div onClick={(): void => handleOptionClick(option)} key={option.value}>
+    <div
+      className='hover:bg-sky-100 rounded cursor-pointer p-1'
+      onClick={(): void => handleOptionClick(option)}
+      key={option.value}>
       {option.label}
     </div>
   ));
 
   return (
-    <div>
+    <div className='w-48 relative' ref={divEl}>
       {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
-      <div onClick={handleClick}>{value?.label || 'Select...'}</div>
-      {isOpen && <div>{renderedOptions}</div>}
+      <Panel
+        className='flex justify-between items-center cursor-pointer'
+        onClick={handleClick}>
+        {value?.label || selectLabel}
+        <GoChevronDown className='text-lg' />
+      </Panel>
+      {isOpen && <Panel className='absolute top-full'>{renderedOptions}</Panel>}
     </div>
   );
 };
